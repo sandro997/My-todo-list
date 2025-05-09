26594a3444d923e6f98abcd879a160aa101758c9
sandro997 <alessandrofavazza97@gmail.com>
2025-05-09 19:12:28 +0200

    Implemented persistent data saving to Local Storage

--- list-section/list_section.js
+++ list-section/list_section.js
@@ -22,20 +22,49 @@ function createListElement() {
   return listElement;
 }
 
+function elementTemplate(taskParent, taskCheck) {
+  const task = createListElement();
+  task.appendChild(taskCheck);
+  task.appendChild(createButton("Done"));
+  task.appendChild(createButton("Delete"));
+  taskParent.appendChild(task);
+}
+
 function createTask() {
   const taskParent = document.querySelector("ul");
   const input = document.querySelector("input");
   const inputValue = input.value;
   const taskCheck = createTaskText(inputValue);
+  const taskData = { myTask: inputValue, completed: false };
+
   if (taskCheck) {
-    const task = createListElement();
-    task.appendChild(taskCheck);
-    task.appendChild(createButton("Done"));
-    task.appendChild(createButton("Delete"));
-    taskParent.appendChild(task);
+    saveTask(taskData);
+    elementTemplate(taskParent, taskCheck);
     input.value = "";
   }
 }
 
 const addButton = document.getElementById("add-button");
 addButton.addEventListener("click", createTask);
+
+let savedTasks = [];
+
+function saveTask(task) {
+  savedTasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(savedTasks));
+}
+
+function loadTasks() {
+  const storedTasks = localStorage.getItem("tasks");
+
+  if (storedTasks) {
+    savedTasks = JSON.parse(storedTasks);
+  }
+  const taskParent = document.querySelector("ul");
+  savedTasks.forEach((taskData) => {
+    const taskCheck = createTaskText(taskData.myTask);
+    elementTemplate(taskParent, taskCheck);
+  });
+}
+
+window.onload = loadTasks;
